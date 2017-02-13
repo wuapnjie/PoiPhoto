eafe9fbe5cab41777396c4fd02c707fa49324296
wuapnjie <wupanjie0611@gmail.com>
2017-02-13 13:00:26 +0800

    Update gradle

--- app/src/main/java/com/xiaopo/flying/photoselecter/ui/MainActivity.java
+++ app/src/main/java/com/xiaopo/flying/photoselecter/ui/MainActivity.java
@@ -27,85 +27,76 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int REQUEST_PHOTO_SELECTED = 100;
+  public static final int REQUEST_PHOTO_SELECTED = 100;
 
-    Button mBtnSelected;
-    @Bind(R.id.preview_pages)
-    ViewPager mPreviewPages;
-    @Bind(R.id.previewList)
-    RecyclerView mPreviewList;
+  Button mBtnSelected;
+  @Bind(R.id.preview_pages) ViewPager mPreviewPages;
+  @Bind(R.id.previewList) RecyclerView mPreviewList;
 
-    private PreviewAdapter mAdapter;
-    private List<String> mPaths;
-    private List<Photo> mPhotos;
+  private PreviewAdapter mAdapter;
+  private List<String> mPaths;
+  private List<Photo> mPhotos;
 
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+    init();
+  }
 
-        init();
+  private void init() {
+    mAdapter = new PreviewAdapter();
+
+    mAdapter.setOnItemClickListener(new PreviewAdapter.OnItemClickListener() {
+      @Override public void onItemClick(View view, int position) {
+        mPreviewPages.setCurrentItem(position);
+      }
+    });
+
+    mPreviewList.setLayoutManager(
+        new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+    mPreviewList.setAdapter(mAdapter);
+
+    mPreviewPages.setAdapter(new PreviewPageAdapter(getSupportFragmentManager()));
+  }
+
+  @OnClick(R.id.btn_selected) public void toPick() {
+    PhotoPicker.newInstance().pick(this);
+    //.inflate(mPreviewList, new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+  }
+
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == RESULT_OK && requestCode == Define.DEFAULT_REQUEST_CODE) {
+      if (mPaths != null) {
+        mPaths.clear();
+      }
+
+      mPaths = data.getStringArrayListExtra(Define.PATHS);
+      for (String path : mPaths) {
+        System.out.println(path);
+      }
+
+      mPhotos = data.getParcelableArrayListExtra(Define.PHOTOS);
+      mAdapter.refershData(mPhotos);
+
+      mPreviewPages.setAdapter(new PreviewPageAdapter(getSupportFragmentManager()));
+    }
+  }
+
+  private class PreviewPageAdapter extends FragmentStatePagerAdapter {
+
+    public PreviewPageAdapter(FragmentManager fm) {
+      super(fm);
     }
 
-    private void init() {
-        mAdapter = new PreviewAdapter();
-
-        mAdapter.setOnItemClickListener(new PreviewAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                mPreviewPages.setCurrentItem(position);
-            }
-        });
-
-        mPreviewList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        mPreviewList.setAdapter(mAdapter);
-
-        mPreviewPages.setAdapter(new PreviewPageAdapter(getSupportFragmentManager()));
+    @Override public Fragment getItem(int position) {
+      return PreviewsFragment.createInstance(mPaths.get(position));
     }
 
-    @OnClick(R.id.btn_selected)
-    public void toPick() {
-        PhotoPicker.newInstance()
-                .inflate(mPreviewList, new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+    @Override public int getCount() {
+      return mPaths == null ? 0 : mPaths.size();
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == Define.DEFAULT_REQUEST_CODE) {
-            if (mPaths != null) {
-                mPaths.clear();
-            }
-
-            mPaths = data.getStringArrayListExtra(Define.PATHS);
-            for (String path : mPaths) {
-                System.out.println(path);
-            }
-
-            mPhotos = data.getParcelableArrayListExtra(Define.PHOTOS);
-            mAdapter.refershData(mPhotos);
-
-            mPreviewPages.setAdapter(new PreviewPageAdapter(getSupportFragmentManager()));
-        }
-
-    }
-
-    private class PreviewPageAdapter extends FragmentStatePagerAdapter {
-
-        public PreviewPageAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return PreviewsFragment.createInstance(mPaths.get(position));
-        }
-
-        @Override
-        public int getCount() {
-            return mPaths == null ? 0 : mPaths.size();
-        }
-    }
+  }
 }
